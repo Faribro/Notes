29fe6c41310e3640610a9ec652b47dd8f96c21c7
Faribro <faridsayyed1010@gmail.com>
2023-05-28 19:53:34 +0530

    New Commit

--- app/api/users/[id]/posts/route.js
+++ app/api/users/[id]/posts/route.js
@@ -5,10 +5,10 @@ export const GET = async (request, { params }) => {
     try {
         await connectToDB()
 
-        const prompts = await Prompt.find({ creator: params.id }).populate("creator")
+        const Notes = await Prompt.find({ creator: params.id }).populate("creator")
 
-        return new Response(JSON.stringify(prompts), { status: 200 })
+        return new Response(JSON.stringify(Notes), { status: 200 })
     } catch (error) {
-        return new Response("Failed to fetch prompts created by user", { status: 500 })
+        return new Response("Failed to fetch Notes created by user", { status: 500 })
     }
-} 
+}
